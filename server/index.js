0c2a8fa1f3892649b2758daae2f7f97f96bc1350
Randy Dang <randy.dang05@gmail.com>
2019-11-13 08:39:05 -0800

    added test config for mongo

--- server/index.js
+++ server/index.js
@@ -3,10 +3,29 @@ require("dotenv").config();
 const express = require("express");
 const secretKey = process.env.APIKEY;
 const port = process.env.PORT || 5000;
-const mongo = require("mongodb").MongoClient;
+const MongoClient = require("mongodb").MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-xwu4t.mongodb.net/test?retryWrites=true&w=majority`;
 
+// const mongoPort = 'mongodb://localhost:27017/mydb'
+
 const app = express();
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+client.connect((err, db) => {
+  if (err) throw err;
+
+  const dbo = db.db("mydb");
+
+  dbo.createCollection("customers", (err, res) => {
+    if (err) throw err;
+    console.log("Collection created!");
+    db.close();
+  });
+});
 
 const root = {
   message: () => "Hello World"
