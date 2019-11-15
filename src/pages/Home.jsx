da9421d142ca2ecc17171059be70cb97f7efd0ff
Randy Dang <randy.dang05@gmail.com>
2019-11-15 09:50:40 -0800

    added login inputs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,42 @@
 import React from "react";
 import AppContainer from "../components/AppContainer/AppContainer";
+import Boba from "../shared/images/boba.svg";
+import "./pages.scss";
 
-const HomePage = () => {
+const HomePage = ({ history, match, ...rest }) => {
+  function signupClick(e) {
+    history.push("/sign-up");
+  }
+
+  function loginClick(e) {
+    history.push("/login");
+  }
+
   return (
     <AppContainer
       title="Boba Tracker"
       render={() => {
         return (
-          <div>
-            <h1>hi</h1>
+          <div styleName="container">
+            <div styleName="img-container">
+              <div styleName="img">
+                <img src={Boba} alt="Milk Tea" />
+              </div>
+            </div>
+            <p styleName="instructions">
+              Use this tool to help keep track how much Boba you intake
+            </p>
+
+            <div styleName="button-container">
+              <div styleName="button">
+                <button onClick={signupClick}>Sign Up</button>
+              </div>
+              <div styleName="button">
+                <button onClick={loginClick}>Login</button>
+              </div>
+            </div>
+
+            <p></p>
           </div>
         );
       }}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import AppContainer from "../components/AppContainer/AppContainer";
+import { TextField, Button, ButtonBase } from "@material-ui/core";
+import styles from "./pages.scss";
 
 const Login = () => {
   return (
     <AppContainer
-      title="Login"
+      title="Login to Boba Tracker"
       render={() => {
         return (
-          <div>
-            <h1>hey</h1>
-          </div>
+          <form styleName="login-container">
+            <TextField styleName="login-input" placeholder="Email Address" />
+            <TextField styleName="login-input" placeholder="Password" />
+            <Button size="large" variant="contained" color="primary">
+              Login
+            </Button>
+          </form>
         );
       }}
     />
