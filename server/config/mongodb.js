db0ab464835032800fda1913c5d2a54bd2a8a93e
dangrt5 <randy.dang05@gmail.com>
2019-11-19 09:43:46 -0800

    replaced db vars with env constants

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,19 +1,13 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-xwu4t.mongodb.net/test?retryWrites=true&w=majority`;
-
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// });
+const uri = process.env.DB_URI;
+const db = mongoose.connection;
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  dbName: "boba_tracker"
+  dbName: process.env.DB_NAME
 });
-
-const db = mongoose.connection;
 
 db.once("open", () => console.log("OPENED"));
 
