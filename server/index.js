9b56939dd8e41b8911598d28267403e3cc5faea8
dangrt5 <randy.dang05@gmail.com>
2020-01-17 11:34:11 -0800

    heroku config

--- server/index.js
+++ server/index.js
@@ -4,10 +4,13 @@ const routes = require("./routes/index.route");
 const db = require("./config/mongodb");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const { join } = require("path");
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(express.static(join(__dirname, "..", "dist")));
 
 // Express Configuration
 
@@ -19,6 +22,10 @@ app.use(passport.initialize());
 // Mount the routes
 
 app.use("/api", routes);
+
+app.get("*", (req, res) => {
+  res.sendFile(join(__dirname, +"../dist/index.html"));
+});
 
 // Listen at certain port
 
